54f6639b4ef386de098d8f131dfaa055b8d30c62
jc-cole <jaredcole@umass.edu>
2025-02-15 14:13:06 -0500

    add basic "course" class

---
+++ course.js
@@ -0,0 +1,9 @@
+export class Course {
+    constructor(name, courseNumber, commentList) {
+        // Ex: Data Structures
+        this.name = name
+        // Ex: CICS 210
+        this.courseNumber = courseNumber
+        this.commentList = commentList
+    }
+}
